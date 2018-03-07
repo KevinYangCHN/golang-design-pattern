d3071aa93c4d5bcb4548e9fa6c3f1b43d2d015ee
杨辰凯 <yangck@wangsu.com>
2018-03-07 10:24:12 +0800

    mediator

---
+++ behavioral_patterns/mediator/mediator.go
@@ -0,0 +1,75 @@
+package mediator
+
+import (
+	"fmt"
+	"strings"
+)
+
+type CDDriver struct {
+	Data string
+}
+
+func (c *CDDriver) ReadData() {
+	c.Data = "music,image"
+	fmt.Printf("CDDriver: reading data %s\n", c.Data)
+	GetMediatorInstance().changed(c)
+}
+
+type Mediator struct {
+	CD    *CDDriver
+	CPU   *CPU
+	Video *VideoCard
+	Sound *SoundCard
+}
+
+type CPU struct {
+	Video string
+	Sound string
+}
+
+func (c *CPU) Process(data string) {
+	sp := strings.Split(data, ",")
+	c.Sound = sp[0]
+	c.Video = sp[1]
+	fmt.Printf("CPU: split data with Sound %s, Video %s\n", c.Sound, c.Video)
+	GetMediatorInstance().changed(c)
+}
+
+type VideoCard struct {
+	Data string
+}
+
+func (c *VideoCard) Display(data string) {
+	c.Data = data
+	fmt.Printf("VideoCard: display %s\n", c.Data)
+	GetMediatorInstance().changed(c)
+}
+
+type SoundCard struct {
+	Data string
+}
+
+func (c *SoundCard) Play(data string) {
+	c.Data = data
+	fmt.Printf("SoundCard: play %s\n", c.Data)
+	GetMediatorInstance().changed(c)
+}
+
+var mediator *Mediator
+
+func GetMediatorInstance() *Mediator {
+	if mediator == nil {
+		mediator = &Mediator{}
+	}
+	return mediator
+}
+
+func (m *Mediator) changed(i interface{}) {
+	switch inst := i.(type) {
+	case *CDDriver:
+		m.CPU.Process(inst.Data)
+	case *CPU:
+		m.Video.Display(inst.Video)
+		m.Sound.Play(inst.Sound)
+	}
+}
